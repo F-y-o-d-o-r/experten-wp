dd421083673a9f7fa7dd2a18605faf8af06ded39
Fyodor <fyodor.work@gmail.com>
2019-06-20 19:17:01 +0300

    backup + db

--- wp-content/themes/experten/front-page.php
+++ wp-content/themes/experten/front-page.php
@@ -475,6 +475,28 @@ get_header();
             </div>
         </div>
     </div>
+
+    <!-- Modals - sent -->
+    <div class="modal calc-form-modal fade" id="modalSent1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true"></span>
+                </button>
+                <div class="modal-header">
+                    <?php
+                    if ($formulartitel_berechnen) { ?>
+                        <h2 class="modal-title" id="exampleModalLabel"><?php echo $formulartitel_berechnen?></h2>
+                        <?php
+                    }
+                    ?>
+                </div>
+                <div class="modal-body">
+                    <?php echo do_shortcode( '[contact-form-7 id="107" title="Berechnen two"]' ); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
 
 get_footer();
